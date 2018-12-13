141392f415018b4900c68dff65e1a26afd992716
n0fail <suuupport2@gmail.com>
2018-12-13 22:41:19 +0300

    first commit

---
+++ paint/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import glossary
+
+admin.site.register(glossary)
---
+++ paint/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class glossary(models.Model):
+    name = models.CharField(max_length=200)
+    words = models.TextField()
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
