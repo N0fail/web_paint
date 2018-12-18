2042c8f137fac4ea43907bd10ce8cefd73e8ccc3
n0fail <suuupport2@gmail.com>
2018-12-18 10:58:38 +0300

    fixed url bug

--- paint/urls.py
+++ paint/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^login$', views.login, name='login'),
     url(r'^room', views.room, name='room'),
     url(r'^fetch', views.fetch_handle, name='fetch_handle'),
-    url(r'', views.login_redirect, name='login_redirect')
+    url(r'^$', views.login_redirect, name='login_redirect')
 ]
